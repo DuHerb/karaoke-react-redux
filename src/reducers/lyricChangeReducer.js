83730c092b2c49ec8ab33058435079ad7acde395
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-22 23:13:00 -0700

    push finished lesson to repo

--- src/reducers/lyricChangeReducer.js
+++ src/reducers/lyricChangeReducer.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-case-declarations */
 import constants from './../constants';
 const { initialState, types } = constants;
 
@@ -21,6 +23,27 @@ const lyricChangeReducer = (state = initialState.songsById, action) => {
     newSongsByIdStateSlice = Object.assign({}, state, {
       [action.currentSongId]: newSongsByIdEntry
     });
+    return newSongsByIdStateSlice;
+  case types.REQUEST_SONG:
+    newSongsByIdEntry = {
+      isFetching: true,
+      title: action.title,
+      songId: action.songId
+    };
+    newSongsByIdStateSlice = Object.assign({}, state, {
+      [action.songId]: newSongsByIdEntry
+    });
+    return newSongsByIdStateSlice;
+  case types.RECIEVE_SONG:
+    newSongsByIdEntry = Object.assign({}, state[action.songId], {
+      isFetching: false,
+      recievedAt: action.recievedAt,
+      title: action.title,
+      songArray: action.songArray,
+      arrayPosition: 0,
+      songId: action.songId
+    });
+    newSongsByIdStateSlice = Object.assign({}, state, { [ action.songId]: newSongsByIdEntry});
     return newSongsByIdStateSlice;
   default:
     return state;
